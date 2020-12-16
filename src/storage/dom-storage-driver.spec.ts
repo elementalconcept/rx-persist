5feff906d6b7b1a60b9b34ad6b4d89b9d94c678e
Aleksandr 'Aux' Ivanov <aux@hexmode.org>
2020-12-16 17:12:19 +0000

    Finished tests for DOMStorageDriver

--- src/storage/dom-storage-driver.spec.ts
+++ src/storage/dom-storage-driver.spec.ts
@@ -1,8 +1,55 @@
-import { localStorageDriver, sessionStorageDriver } from './dom-storage-driver';
+import { DOMStorageDriver, localStorageDriver, sessionStorageDriver } from './dom-storage-driver';
+
+class StorageMock implements Storage {
+  private readonly storage: any = {};
+
+  readonly length = 0;
+
+  clear(): void {
+  }
+
+  getItem(key: string): string | null {
+    return this.storage[ key ] === undefined ? null : this.storage[ key ];
+  }
+
+  key(index: number): string | null {
+    return null;
+  }
+
+  removeItem(key: string): void {
+    delete this.storage[ key ];
+  }
+
+  setItem(key: string, value: string): void {
+    this.storage[ key ] = value;
+  }
+}
 
 describe('DOMStorageDriver', () => {
-  describe('methods', () => {
+  let mock: Storage;
+  let driver: DOMStorageDriver;
 
+  beforeEach(() => {
+    mock = new StorageMock();
+    driver = new DOMStorageDriver(mock);
+  });
+
+  describe('methods', () => {
+    it('should store value in the storage', () => {
+      driver.set('getTest', 'value');
+      expect(mock.getItem('getTest')).toBe(JSON.stringify('value'));
+    });
+
+    it('should retrieve value from storage', () => {
+      mock.setItem('setTest', JSON.stringify('new value'));
+      expect(driver.get('setTest')).toBe('new value');
+    });
+
+    it('should remove value from storage', () => {
+      mock.setItem('removeTest', JSON.stringify('removed value'));
+      driver.remove('removeTest');
+      expect(mock.getItem('removeTest')).toBeNull();
+    });
   });
 
   describe('exports', () => {
