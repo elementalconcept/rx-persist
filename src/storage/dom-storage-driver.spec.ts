ec761fb967ce87dc53d41721f76611be7350265e
Aleksandr 'Aux' Ivanov <aux@hexmode.org>
2020-12-16 17:28:48 +0000

    Added tests for persistent

--- src/storage/dom-storage-driver.spec.ts
+++ src/storage/dom-storage-driver.spec.ts
@@ -1,54 +1,30 @@
 import { DOMStorageDriver, localStorageDriver, sessionStorageDriver } from './dom-storage-driver';
-
-class StorageMock implements Storage {
-  private readonly storage: any = {};
-
-  readonly length = 0;
-
-  clear(): void {
-  }
-
-  getItem(key: string): string | null {
-    return this.storage[ key ] === undefined ? null : this.storage[ key ];
-  }
-
-  key(index: number): string | null {
-    return null;
-  }
-
-  removeItem(key: string): void {
-    delete this.storage[ key ];
-  }
-
-  setItem(key: string, value: string): void {
-    this.storage[ key ] = value;
-  }
-}
+import { StorageMock } from '../internal/mocks/storage-mock';
 
 describe('DOMStorageDriver', () => {
-  let mock: Storage;
+  let storageMock: Storage;
   let driver: DOMStorageDriver;
 
   beforeEach(() => {
-    mock = new StorageMock();
-    driver = new DOMStorageDriver(mock);
+    storageMock = new StorageMock();
+    driver = new DOMStorageDriver(storageMock);
   });
 
   describe('methods', () => {
     it('should store value in the storage', () => {
       driver.set('getTest', 'value');
-      expect(mock.getItem('getTest')).toBe(JSON.stringify('value'));
+      expect(storageMock.getItem('getTest')).toBe(JSON.stringify('value'));
     });
 
     it('should retrieve value from storage', () => {
-      mock.setItem('setTest', JSON.stringify('new value'));
+      storageMock.setItem('setTest', JSON.stringify('new value'));
       expect(driver.get('setTest')).toBe('new value');
     });
 
     it('should remove value from storage', () => {
-      mock.setItem('removeTest', JSON.stringify('removed value'));
+      storageMock.setItem('removeTest', JSON.stringify('removed value'));
       driver.remove('removeTest');
-      expect(mock.getItem('removeTest')).toBeNull();
+      expect(storageMock.getItem('removeTest')).toBeNull();
     });
   });
 
