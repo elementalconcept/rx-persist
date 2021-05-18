06e7b72430df8015192161d57168b7ac475cd3c5
Aleksandr 'Aux' Ivanov <aux@hexmode.org>
2021-05-18 18:40:06 +0100

    Added currentVersion to VersionedOptions

--- src/models/versioned-options.ts
+++ src/models/versioned-options.ts
@@ -1,6 +1,7 @@
 import { StorageDriver } from './storage-driver';
 
 export interface VersionedOptions {
+  currentVersion: number;
   versionKey: string | string[];
   migrate: (version: number, value: any) => any;
 
